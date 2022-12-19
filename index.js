ba5adc061256f3a37b0596fc44092f2eade89f76
Leonardo-Braga <54039202+Elelebe@users.noreply.github.com>
2022-12-19 17:44:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises;
 async function start(){
     const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
-    const pokemon = 'Pikachu'
+    const pokemon = 'pokemon-name'
 
     await page.goto('https://pokemon.fandom.com/pt-br/wiki/Pokédex_Nacional')
 
